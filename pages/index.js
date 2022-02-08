ff712481b656874e1ae389ec4353f52c3d5eac6f
Keerthanen <keerthanenr@live.co.uk>
2022-02-08 22:21:25 +0000

    New Project

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          What is a&nbsp;
+          <a className="text-blue-600" href="https://www.linkedin.com/in/keerthanen-ravichandran-b49780102/">
+             PM?
+          </a>
+        </h1>
+
+      </main>
+    </div>
+  )
+}

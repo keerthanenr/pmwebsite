ce50d1fa8bbc6b4d7ef5c65f2fd2e1a57b3fa929
Keerthanen <keerthanenr@live.co.uk>
2022-02-08 22:40:00 +0000

    Updating index page

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          What is a&nbsp;
-          <a className="text-blue-600" href="https://www.linkedin.com/in/keerthanen-ravichandran-b49780102/">
-             PM?
-          </a>
+          New website under construction&nbsp;
         </h1>
-
+        <h3 className="text-6xl font-bold text-red-400" >Website to answer- what is a PM?</h3>
+        <h2 className="text-6xl font-bold text-blue-400">
+             <a  href="https://www.linkedin.com/in/keerthanen-ravichandran-b49780102/">Contact me</a>
+          </h2>
       </main>
     </div>
   )
